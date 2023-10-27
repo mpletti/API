32994ad3de444119796daffdfa965c42f8e8140e
mpletti <mppasqualetti@hotmail.com>
2023-10-26 22:12:15 -0300

    update express

--- backend/src/server.js
+++ backend/src/server.js
@@ -5,3 +5,5 @@ const app = express();
 app.get("/", (request, response) => response.status(200).send('Hello'));
 
 app.listen(3000, () => console.log('Server running or port 3000'));
+
+module.exports = app;
